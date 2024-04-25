8df03bd285ba6d3fd9bdcd58e782de889eb7030a
Dustin Osborne <dustinkosborne@gmail.com>
2024-04-24 20:20:47 -0400

    +

--- ClearVision-API/Messaging/Function.cs
+++ ClearVision-API/Messaging/Function.cs
@@ -1,36 +1,40 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Azure.Communication.Identity;
 
 namespace ClearVision_API.Messaging
 {
     public static class Function
     {
-        [FunctionName("Function")]
-        [Route("[controller]")]
+        [FunctionName("GenerateToken")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            // Initialize communication client
+            string connectionString = Environment.GetEnvironmentVariable("ACS_CONNECTION_STRING");
+            var client = new CommunicationIdentityClient(connectionString);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // Create a user and token
+            var tokenResponse = await client.CreateUserAndTokenAsync(scopes: new[] { CommunicationTokenScope.Chat });
+            var user = tokenResponse.Value.User;
+            var token = tokenResponse.Value.AccessToken;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            var result = new
+            {
+                userId = user.Id,
+                token = token.Token,
+                expiresOn = token.ExpiresOn
+            };
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(result);
         }
     }
 }
